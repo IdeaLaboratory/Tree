5bc3f2dc2e193fc204da6e420b016452d1f91b3f
AdiTCG <aditheorigin@gmail.com>
2016-06-27 00:09:59 +0530

    Implemented Insert method to create new node of a tree.

--- BinarySearchTree/BinarySearchTree/BST.cpp
+++ BinarySearchTree/BinarySearchTree/BST.cpp
@@ -1,3 +1,40 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+	int val;
+	Node *left;
+	Node *right;
+};
+
+Node* CreateNode(int p_val)
+{
+	Node *newNode = new Node();
+	newNode->val = p_val;
+	newNode->left = newNode->right = NULL;
+	return newNode;
+}
+void InsertNode(Node **p_root, int p_val)
+{
+	if (*p_root == NULL)
+	{
+		*p_root = CreateNode(p_val);
+		return;
+	}
+	if ((*p_root)->val >= p_val)
+		InsertNode(&(*p_root)->left, p_val);
+	else
+		InsertNode(&(*p_root)->right, p_val);
+}
+
+
 //Very first step and Simplest c++ code
 int main()
-{}
+{
+	Node *root;
+	root = NULL;
+	InsertNode(&root, 25);
+	InsertNode(&root, 10);
+	InsertNode(&root, 35);
+	InsertNode(&root, 5);
+}
