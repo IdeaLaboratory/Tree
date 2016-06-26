1509158f4b6173e301ad4b9896703faff179fc39
AdiTCG <aditheorigin@gmail.com>
2016-06-27 00:23:22 +0530

    Implemented basci Binary Search

--- BinarySearchTree/BinarySearchTree/BST.cpp
+++ BinarySearchTree/BinarySearchTree/BST.cpp
@@ -21,10 +21,23 @@ void InsertNode(Node **p_root, int p_val)
 		*p_root = CreateNode(p_val);
 		return;
 	}
+
 	if ((*p_root)->val >= p_val)
 		InsertNode(&(*p_root)->left, p_val);
 	else
 		InsertNode(&(*p_root)->right, p_val);
+}
+
+Node* Search(Node* p_root, int p_val)
+{
+	if (p_root == NULL)
+		return NULL;
+	else if (p_root->val == p_val)
+		return p_root;
+	else if (p_root->val > p_val)
+		Search((p_root)->left, p_val);
+	else
+		Search((p_root)->right, p_val);
 }
 
 
@@ -37,4 +50,6 @@ int main()
 	InsertNode(&root, 10);
 	InsertNode(&root, 35);
 	InsertNode(&root, 5);
+
+	Node *expNode = Search(root, 10);
 }
